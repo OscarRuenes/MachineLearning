1ed3637ec6c235653feccd08242a716d48d4ca95
OscarRuenes <155939692+OscarRuenes@users.noreply.github.com>
2025-09-21 01:16:55 -0500

    Create main.py
    Create repo and main.py

---
+++ BayesianLearning/main.py
@@ -0,0 +1,7 @@
+'''
+Group Members: Lalith Vennapusa, Oscar Ruenes
+Date: 9/21/2025
+Title: CS 4375 Assignment 2 Bayesian Learning
+Desc: Naive Bayes learning algorithm for binary classification tasks, assumes no missing values for features and binary feature values. Takes data from train.dat and test.dat files, with a header line for the
+titles of features/classification and further lines containing the binary values of each feature and classification. Prints training set and test set accuracies.
+'''
